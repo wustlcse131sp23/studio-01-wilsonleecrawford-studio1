c476d15bf3be4c580354b2bba902e17332d3c240
lerawilson <lerawilson@localhost>
2023-01-26 13:53:58 -0600

    studio1 - complete

---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,19 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.println("Input year:");
+		int year = in.nextInt();
+		boolean leapYear = (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0);
+		System.out.println(year + "is leap year: " + leapYear); 
+		
+		
+		
+	}
+
+}
--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,7 +15,9 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
-
+		
+		boolean isOrdered = (x < y) && (y < z) || (x > y) && (y > z);
+		System.out.println(isOrdered);
 	}
 
 }
